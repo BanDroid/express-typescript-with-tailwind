7085ce67ed50eb8894e34f76f50136c26930393b
Fitrah Nuno Syahbani <68449470+BanDroid@users.noreply.github.com>
2023-05-16 16:53:23 +0700

    remove test route

--- src/routes/pages/index.ts
+++ src/routes/pages/index.ts
@@ -5,8 +5,5 @@ const pages = Router();
 pages.get("/", (req, res) => {
 	res.render("pages/home");
 });
-pages.get("/hello", (req, res) => {
-	res.render("pages/test");
-});
 
 export default pages;
